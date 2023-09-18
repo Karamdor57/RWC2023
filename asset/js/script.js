6936da86544b6a0a1de369a926834671bbbc9693
Antoine <121904586+Karamdor57@users.noreply.github.com>
2023-09-18 11:11:58 +0200

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,7 +5,7 @@ const selectors = {
     timer: document.querySelector('.timer'),
     start: document.querySelector('#start-button'),
     win: document.querySelector('.win'),
-    restart: document.querySelector('#restart-button'),
+    restart: document.querySelector('#restartButton'),
     infoForm: document.getElementById('info-form'),
     submitInfoButton: document.getElementById('submit-info-button')
 }
@@ -52,7 +52,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
     
-    const emojis = ['<img src="../RWC2023/rugbylogo/Fiji_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Afrique_du_sud_logo_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Angleterre_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Argentine_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Chilie_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Scottish_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/france_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Irish_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Italie_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Japon_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Nouvelle-Zelande_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Georgie_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Namibia_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Portugal_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Roumania_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Samoa_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Tonga_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Uruguay_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Wales_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Wallabies_Rugby.png" alt="">',]
+    const emojis = ['<img src="../RWC2023/rugbylogo/Fiji_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Afrique_du_sud_logo_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Angleterre_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Argentine_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Chilie_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Scottish_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/France_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Irish_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Italie_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Japon_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Nouvelle-Zelande_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Georgie_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Namibia_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Portugal_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Roumania_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Samoa_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Tonga_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Uruguay_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Wales_Rugby.png" alt="">','<img src="../RWC2023/rugbylogo/Wallabies_Rugby.png" alt="">',]
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
@@ -87,25 +87,34 @@ const startGame = () => {
         selectors.timer.innerText = `Time: ${state.totalTime} sec`
     }, 1000)
 }
-const resetGame = () => {
-    // Réinitialiser l'état
-    state.gameStarted = false;
-    state.flippedCards = 0;
-    state.totalFlips = 0;
-    state.totalTime = 0;
-    clearInterval(state.loop);
+// const resetGame = () => {
+//     // Réinitialiser l'état
+//     state.gameStarted = false;
+//     state.flippedCards = 0;
+//     state.totalFlips = 0;
+//     state.totalTime = 0;
+//     clearInterval(state.loop);
 
-    // Mettre à jour l'interface utilisateur
-    selectors.moves.innerText = `0 moves`;
-    selectors.timer.innerText = `Time: 0 sec`;
-    selectors.start.classList.remove('disabled');
-    selectors.restart.classList.add('disabled');
+//     // Mettre à jour l'interface utilisateur
+//     selectors.moves.innerText = `0 moves`;
+//     selectors.timer.innerText = `Time: 0 sec`;
+//     selectors.start.classList.remove('disabled');
+//     selectors.restart.classList.add('disabled');
 
-    // Générer un nouveau tableau de jeu
-    generateGame();
-};
+//     // Générer un nouveau tableau de jeu
+//     generateGame();
+// };
 
-
+document.addEventListener("DOMContentLoaded", function() {
+    // Trouver le bouton par son identifiant
+    const restartButton = document.getElementById("restartButton");
+  
+    // Ajouter un événement "click" au bouton
+    restartButton.addEventListener("click", function() {
+      // Recharge la page
+      location.reload();
+    });
+  });
 const flipBackCards = () => {
     document.querySelectorAll('.card:not(.matched)').forEach(card => {
         card.classList.remove('flipped')
@@ -188,6 +197,10 @@ selectors.submitInfoButton.addEventListener('click', () => {
 
     // Vous pouvez maintenant utiliser playerName et playerEmail comme vous le souhaitez, par exemple, les envoyer à un serveur ou les stocker localement.
 
+    // Stockez les données dans le localStorage
+    localStorage.setItem('playerName', playerName);
+    localStorage.setItem('playerEmail', playerEmail);
+
     // Confirmez que les informations ont été enregistrées
     alert('Informations enregistrées avec succès !');
 
@@ -195,6 +208,18 @@ selectors.submitInfoButton.addEventListener('click', () => {
     selectors.infoForm.style.display = 'none';
 });
 
+// Récupérer les données du localStorage lorsque la page se charge
+window.addEventListener('load', () => {
+    const playerName = localStorage.getItem('playerName');
+    const playerEmail = localStorage.getItem('playerEmail');
+
+    // Vérifier si les valeurs existent
+    if (playerName && playerEmail) {
+        // Vous pouvez utiliser playerName et playerEmail comme bon vous semble ici
+        console.log(`Nom du joueur : ${playerName}`);
+        console.log(`Adresse e-mail du joueur : ${playerEmail}`);
+    }
+});
 // Fonction pour afficher la fenêtre de saisie des informations
 function showInfoForm() {
     selectors.infoForm.style.display = 'block';
